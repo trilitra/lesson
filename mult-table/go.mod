3706282cfd1a3242b227fc1d73ca65e30e7b9ed3
Alex Efanov <ligawowa@gmail.com>
2026-02-04 14:39:29 +0500

    Add files via upload

---
+++ mult-table/go.mod
@@ -0,0 +1,1 @@
+module mult-table
---
+++ mult-table/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	numbers := [...]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	fmt.Print("a\\b\t")
+
+	for _, a := range numbers {
+		fmt.Printf("%4d", a)
+	}
+	fmt.Print("\n\n")
+
+	for _, a := range numbers {
+		fmt.Printf("%d\t", a)
+
+		for _, b := range numbers {
+			result := a * b
+			fmt.Printf("%4d", result)
+		}
+		fmt.Println()
+	}
+}
